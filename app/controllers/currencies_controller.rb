586f91033ee01cf997353d4ff9edcad501c256ca
Baggio <shehadibaggio@gmail.com>
2019-09-24 13:46:51 -0500

    finished backend starting react

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -3,9 +3,24 @@ class CurrenciesController < ApplicationController
 	end
 	
 	def search
+		@currencies = Currencies.where('LOWER(name) LIKE ?',"%#{params[:search].downcase}%")
+		render json: {currencies: @currencies}
 	end
 
+	#Takes in currency id and the amount owned
+	#Returns final calculation
 	def calculate
+		amount = params[:amount]
+		render json: {
+			currency: currency,
+			current_price: currency.current_price,
+			amount: amount,
+			value: currency.calculate_value(amount)
+		}
 	end
-	
+
+	private
+	def currency
+		@currency || = Currency.find(params[:id])
+	end
 end
