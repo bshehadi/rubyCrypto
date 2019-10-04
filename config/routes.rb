a4d321c5807e99268ac89ee36191fb2c247fe508
Baggio <shehadibaggio@gmail.com>
2019-10-04 09:51:23 -0500

    user model and sessions controller ref #2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 	get "api/test", to: "static#home"
 	post "search", to: "currencies#search"
 	post "calculate", to: "currencies#calculate"
+	resources :sessions, only: [:create]
+	resources :registrations, only: [:create]
+	delete :logout, to: "sessions#logout"
+	get :logged_in, to: "sessions#logged_in"
  end
