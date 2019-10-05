c09440a87a8597bc41517a6dc713871ba86cc964
Baggio <shehadibaggio@gmail.com>
2019-10-05 17:38:34 -0500

    another deployment bug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === "production"
-	Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "bes-authapp.herokuapp.com"
+	Rails.application.config.session_store :cookie_store, key: "_auth_app", domain: "quiet-tor-17270.herokuapp.com"
 else
 	Rails.application.config.session_store :cookie_store, key: "_auth_app"
 end
